4537137fb08578f1eb68cd5437a588098587bebe
Nikunj Mahawar <90510801+NikeMahawar@users.noreply.github.com>
2024-06-08 12:44:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 function loco(){
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  },
   getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
 
@@ -26,7 +21,6 @@ ScrollTrigger.scrollerProxy("#main", {
 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 }
 loco()
@@ -455,7 +449,6 @@ gsap.to(imageSeq, {
   scrollTrigger: {
     scrub: 0.15,
     trigger: `#page7>canvas`,
-    //   set start end according to preference
     start: `top top`,
     end: `600% top`,
     scroller: `#main`,
@@ -495,7 +488,6 @@ ScrollTrigger.create({
   pin: true,
   // markers:true,
   scroller: `#main`,
-//   set start end according to preference
   start: `top top`,
   end: `600% top`,
 });
